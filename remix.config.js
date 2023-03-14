fe43d2556f1ceb0f315a9a4c2f8194f7c3c24ac5
Maksim Perkov <kostarsf@gmail.com>
2023-03-14 21:18:32 +0300

    Try another config

--- remix.config.js
+++ remix.config.js
@@ -7,6 +7,8 @@ module.exports = {
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
+  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
+  serverBuildPath: "api/index.js",
   future: {
     unstable_tailwind: true,
     v2_routeConvention: true,
